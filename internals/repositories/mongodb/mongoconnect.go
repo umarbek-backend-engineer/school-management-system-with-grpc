10b979b5e0ea97b7c475461680c7a8b9cf440647
umarbek-backend-engineer <259284488+umarbek-backend-engineer@users.noreply.github.com>
2026-02-04 19:25:35 +0500

    Implementation of error handler

--- internals/repositories/mongodb/mongoconnect.go
+++ internals/repositories/mongodb/mongoconnect.go
@@ -3,6 +3,7 @@ package mongodb
 import (
 	"context"
 	"log"
+	"school_project_grpc/pkg/utils"
 
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
@@ -10,16 +11,16 @@ import (
 
 func CreatMongoClient() (*mongo.Client, error) {
 	ctx := context.Background()
-	
+
 	// client, err := mongo.Connect(ctx, options.Client().ApplyURI("username:password@mongodb://localhost:27017"))
 	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
 	if err != nil {
-		return nil, err
+		return nil, utils.ErrorHandler(err, "Failed to connect to DataBase")
 	}
 
 	err = client.Ping(ctx, nil)
 	if err != nil {
-		return nil, err
+		return nil, utils.ErrorHandler(err, "Failed to ping to DataBase")
 	}
 
 	log.Println("🎉 mongoDB connected successfully")
