3f955d07618a15a3c7e3fd4a64781802cb4a5773
umarbek-backend-engineer <259284488+umarbek-backend-engineer@users.noreply.github.com>
2026-02-08 22:02:27 +0500

    Implemented TLS/SSL

--- cmd/grpcapi/server.go
+++ cmd/grpcapi/server.go
@@ -8,6 +8,7 @@ import (
 
 	"school_project_grpc/internals/api/handlers"
 	itc "school_project_grpc/internals/api/interceptors"
+	"school_project_grpc/pkg/utils"
 	pb "school_project_grpc/proto/gen"
 
 	"github.com/joho/godotenv"
@@ -26,6 +27,13 @@ func main() {
 	log.Println("🎉 .env file successfully loaded")
 
 	port := os.Getenv("GRPC_SERVER_PORT")
+	// cert := os.Getenv("CERT_FILE")
+	// key := os.Getenv("KEY_FILE")
+
+	// creds, err := credentials.NewClientTLSFromFile(cert, key)
+	// if err != nil {
+	// 	log.Fatalf("Failed to load TLS cert files")
+	// }
 
 	lis, err := net.Listen("tcp", port)
 	if err != nil {
@@ -34,6 +42,7 @@ func main() {
 	defer lis.Close()
 
 	// registering grpcServer, this is essential to run the server
+	// grpcServer := grpc.NewServer(grpc.ChainUnaryInterceptor(itc.NewRateLimiter(20, time.Second*10).RateLimitIntercepter, itc.ResponseTimeIntercepter, itc.Authentication_Intercepter), grpc.Creds(creds))
 	grpcServer := grpc.NewServer(grpc.ChainUnaryInterceptor(itc.NewRateLimiter(20, time.Second*10).RateLimitIntercepter, itc.ResponseTimeIntercepter, itc.Authentication_Intercepter))
 
 	// registering rpcs
@@ -43,6 +52,8 @@ func main() {
 
 	// this function is responsible to skip the proto file when testing in postman, it is only used in production period to test
 	reflection.Register(grpcServer)
+
+	go utils.JwtStore.CleanUpExpiredTokens()
 
 	log.Println("Server is running on port", port)
 	log.Println("--------------------------------------")
